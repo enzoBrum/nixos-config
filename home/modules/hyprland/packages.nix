b0aa2bae94ece77c7abc4550f4acad576e750c4b
Enzo Brum <darosabrumenzo@gmail.com>
2024-01-21 00:10:34 -0300

    Create script for changing wallpaper

--- home/modules/hyprland/packages.nix
+++ home/modules/hyprland/packages.nix
@@ -1,5 +1,20 @@
-{ config, pkgs, pkgs-stable, ... }: {
+{ config, pkgs, pkgs-stable, ... }:
+let
+  chwall = pkgs.writeScriptBin "chwall" /* bash */ ''
+    #!/usr/bin/env bash
+    
+    if [ "$#" -ne 1 ]; then
+      echo "Usage: chwall <path-to-wallpaper>"
+      exit 1
+    fi
+    swww img $1
+    wal -s -n -i $1
+    echo "wallpaper changed" | socat - UNIX-CONNECT:/tmp/color_server.sock
+  ''; 
+in
+{
   home.packages = with pkgs; [
+    chwall
     wl-clipboard
     mako
     dmenu
