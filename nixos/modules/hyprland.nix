810b09cc5e639dd241051c0e26255679a5dad581
Enzo Brum <darosabrumenzo@gmail.com>
2024-01-29 22:02:31 -0300

    disable gpg-agent suport for ssh

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -4,10 +4,10 @@
   programs.hyprland.xwayland.enable = true;
 
   programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
   security.pam.services.swaylock = { };
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
