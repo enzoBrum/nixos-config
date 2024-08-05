178fd5c8b6d3fe2dc52825ccc58b7dd178987e89
Enzo Brum <darosabrumenzo@gmail.com>
2024-08-05 17:41:25 -0300

    remove rocket-chat from flatpak

--- scripts/flatpaks.sh
+++ scripts/flatpaks.sh
@@ -2,5 +2,4 @@
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak update
-flatpak install flathub chat.rocket.RocketChat -y
 flatpak install flathub com.github.tchx84.Flatseal -y
