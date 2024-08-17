3d38899480fe923a1ccbba627bb777b07e2fa3fd
Enzo Brum <darosabrumenzo@gmail.com>
2024-08-17 20:45:41 -0300

    remove swaync macchiato theme

--- home/modules/hyprland/swaync.nix
+++ home/modules/hyprland/swaync.nix
@@ -3,9 +3,9 @@ let
   tag = "v0.1.2.1";
 in
 {
-  xdg.configFile."swaync/style.css".text = (builtins.readFile (pkgs.fetchurl
-    {
-      url = "https://github.com/catppuccin/swaync/releases/download/${tag}/macchiato.css";
-      hash = "sha256-6X+KSIYcPRQDGm1YV8rk5mq21Gk/pFCugOEoavh3tBw=";
-    }));
+  #xdg.configFile."swaync/style.css".text = (builtins.readFile (pkgs.fetchurl
+  #  {
+  #    url = "https://github.com/catppuccin/swaync/releases/download/${tag}/macchiato.css";
+  #    hash = "sha256-6X+KSIYcPRQDGm1YV8rk5mq21Gk/pFCugOEoavh3tBw=";
+  #  }));
 }
